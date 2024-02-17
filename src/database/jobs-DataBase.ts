182c341309d7bd66050aa499edf5da8bc0958992
Sherif Awad <eng.sherifawad@gmail.com>
2024-02-17 16:34:48 +0200

    database: can not write to file after deploy

--- src/database/jobs-DataBase.ts
+++ src/database/jobs-DataBase.ts
@@ -1,17 +1,17 @@
 import "server-only";
 
 import { EmployeePosition } from "@/validation/employeeSchema";
-import { promises as fs } from "fs";
-import path from "path";
 
-// let Jobs = require("../data/jobs.json") as EmployeePosition[];
-const dataFilePath = path.join(process.cwd(), "src", "data", "jobs.json");
-const Jobs = (async function Jobs() {
-	return JSON.parse(await fs.readFile(dataFilePath, "utf8"));
-})() as unknown as Promise<EmployeePosition[]>;
+// // let Jobs = require("../data/jobs.json") as EmployeePosition[];
+// const dataFilePath = path.join(process.cwd(), "src", "data", "jobs.json");
+// const Jobs = (async function Jobs() {
+// 	return JSON.parse(await fs.readFile(dataFilePath, "utf8"));
+// })() as unknown as Promise<EmployeePosition[]>;
+
+let Jobs: EmployeePosition[] = require("../data/jobs.json");
 
 export async function getAllJobs(): Promise<EmployeePosition[]> {
-	return await Jobs;
+	return Jobs;
 }
 export async function findByCode(
 	code: number,
